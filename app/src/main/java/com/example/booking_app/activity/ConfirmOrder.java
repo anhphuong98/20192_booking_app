ce4c2c1f2b37f09e0ec09ebf0dd6b03afbca5e44
nganluu <nganltt712@gmail.com>
2020-06-10 14:17:08 +0700

    add detail order

--- app/src/main/java/com/example/booking_app/activity/ConfirmOrder.java
+++ app/src/main/java/com/example/booking_app/activity/ConfirmOrder.java
@@ -1,16 +1,70 @@
 package com.example.booking_app.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.booking_app.R;
+import com.example.booking_app.adapter.ConfirmOrderAdapter;
+import com.example.booking_app.models.dish.CartDish;
+
+import java.util.ArrayList;
 
 public class ConfirmOrder extends AppCompatActivity {
+    ImageView userimg;
+    TextView username, useraddress, store, subtotal, ship, discount, total;
+    TextView subtotalprice, shipfee, discountprice, totalprice;
+    TextView edit;
+    Button submit;
+    RecyclerView listitem;
 
+    ArrayList<CartDish> cartDish = new ArrayList<CartDish>();
+    ConfirmOrderAdapter confirmOrderAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_order);
+
+        init();
+        getItemOrder();
+    }
+    public void init(){
+        //user information
+        userimg = (ImageView) findViewById(R.id.avtuserord);
+        username = (TextView) findViewById(R.id.receivername);
+        useraddress = (TextView) findViewById(R.id.receiveraddress);
+
+        //store
+        store = (TextView) findViewById(R.id.order_store_name);
+
+        //money
+        subtotal = (TextView) findViewById(R.id.subprice);
+        subtotalprice = (TextView) findViewById(R.id.subtotalprice);
+        ship = (TextView) findViewById(R.id.shipfee);
+        shipfee = (TextView) findViewById(R.id.shipfee2);
+        discount = (TextView) findViewById(R.id.discountfee);
+        discountprice = (TextView) findViewById(R.id.discountfee2);
+        total = (TextView) findViewById(R.id.totalprice1);
+        totalprice = (TextView) findViewById(R.id.totalprice2);
+
+        //button
+        edit = (TextView) findViewById(R.id.edit);
+        submit = (Button) findViewById(R.id.submitorder);
+
+        //list item order
+        listitem = (RecyclerView) findViewById(R.id.list_item_order);
+    }
+
+    public void getItemOrder(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        listitem.setLayoutManager(linearLayoutManager);
+        confirmOrderAdapter = new ConfirmOrderAdapter(ConfirmOrder.this, cartDish);
+        listitem.setAdapter(confirmOrderAdapter);
+
     }
 }
---
+++ app/src/main/java/com/example/booking_app/adapter/ConfirmOrderAdapter.java
@@ -0,0 +1,62 @@
+package com.example.booking_app.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.booking_app.R;
+import com.example.booking_app.models.dish.CartDish;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ConfirmOrderAdapter extends RecyclerView.Adapter<ConfirmOrderAdapter.ConfirmorderHolder> {
+    Context context;
+    ArrayList<CartDish> listOrder;
+    @NonNull
+    @Override
+    public ConfirmorderHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.order_list_item, parent, false);
+        ConfirmorderHolder confirmorderHolder = new ConfirmorderHolder(view);
+        return confirmorderHolder;
+    }
+
+    public ConfirmOrderAdapter(Context context, ArrayList<CartDish> listOrder) {
+        this.context = context;
+        this.listOrder = listOrder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ConfirmorderHolder holder, int position) {
+        CartDish cartDish = listOrder.get(position);
+        Picasso.with(context).load(cartDish.getImage()).into(holder.itemimg);
+        holder.name.setText(cartDish.getName());
+        Double price = cartDish.getPrice()*cartDish.getQuantity();
+        holder.price.setText(price.toString());
+        holder.quantity.setText(cartDish.getQuantity());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listOrder.size();
+    }
+
+    public class ConfirmorderHolder extends RecyclerView.ViewHolder {
+        ImageView itemimg;
+        TextView quantity, name, price;
+        public ConfirmorderHolder(@NonNull View itemView) {
+            super(itemView);
+            itemimg = (ImageView) itemView.findViewById(R.id.order_item_img);
+            quantity = (TextView) itemView.findViewById(R.id.order_item_quantity);
+            name = (TextView) itemView.findViewById(R.id.order_item_name);
+            price = (TextView) itemView.findViewById(R.id.order_item_price);
+        }
+    }
+}

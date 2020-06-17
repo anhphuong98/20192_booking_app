f2a8f3cbd71c440202bba34006ad0185a9958598
vuxinh <xinh.vt164737@sis.hust>
2020-06-17 12:20:48 +0700

    search

--- app/src/main/java/com/example/booking_app/adapter/StoreAdapter.java
+++ app/src/main/java/com/example/booking_app/adapter/StoreAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,16 +22,18 @@ import java.util.ArrayList;
 
 import retrofit2.Callback;
 
-public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreHolder> {
+public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreHolder> implements Filterable {
     Context context;
     ArrayList<DataStore> listStore;
+    ArrayList<DataStore> allStore;
     private OnStoreListener mOnStoreListener;
 
 
-    public interface OnStoreListener{
+    public interface OnStoreListener {
         void onStoreClick(int position);
 
     }
+
     public void setOnStoreListener(OnStoreListener onStoreListener) {
         this.mOnStoreListener = onStoreListener;
     }
@@ -37,6 +41,7 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreHolder>
     public StoreAdapter(Context context, ArrayList<DataStore> listStore) {
         this.context = context;
         this.listStore = listStore;
+        allStore = new ArrayList<>(listStore);
 
     }
 
@@ -45,8 +50,8 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreHolder>
     // khởi tạo view cho layout thiết kế bên ngoài
     public StoreHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.store_item, parent,false);
-        StoreHolder storeHolder = new StoreHolder(view,mOnStoreListener);
+        View view = layoutInflater.inflate(R.layout.store_item, parent, false);
+        StoreHolder storeHolder = new StoreHolder(view, mOnStoreListener);
         return storeHolder;
     }
 
@@ -66,19 +71,19 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreHolder>
         return listStore.size();
     }
 
-    public class StoreHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class StoreHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ImageView imageStore;
         TextView nameStore, addressStore;
         OnStoreListener onStoreListener;
 
-        public StoreHolder(@NonNull View itemView, OnStoreListener onStoreListener ) {
+        public StoreHolder(@NonNull View itemView, OnStoreListener onStoreListener) {
             super(itemView);
             imageStore = (ImageView) itemView.findViewById(R.id.imagestore);
             nameStore = (TextView) itemView.findViewById(R.id.namestore);
             addressStore = (TextView) itemView.findViewById(R.id.addressstore);
             itemView.setOnClickListener(this);
             this.onStoreListener = onStoreListener;
-            }
+        }
 
 
         @Override
@@ -86,4 +91,39 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreHolder>
             onStoreListener.onStoreClick(getAbsoluteAdapterPosition());
         }
     }
+
+    @Override
+    public Filter getFilter() {
+        return storeFilter;
+    }
+
+    private Filter storeFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<DataStore> filterStore = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0) {
+                filterStore.addAll(allStore);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for (DataStore store : allStore) {
+                    if (store.getName().toLowerCase().contains(filterPattern)) {
+                        filterStore.add(store);
+                    }
+                }
+
+            }
+            FilterResults results = new FilterResults();
+            results.values = filterStore;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            listStore.clear();
+            listStore.addAll((ArrayList) results.values);
+            notifyDataSetChanged();
+
+        }
+    };
+
 }
